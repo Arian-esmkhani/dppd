c54f3858c36294abca65a7105fb704fb89af19a7
arian <arian.esmkhani@gmail.com>
2025-02-26 12:58:00 +0330

    dppd-ub1-4

--- dppd/src/main/java/com/example/demo/controllers/HelloController.java
+++ dppd/src/main/java/com/example/demo/controllers/HelloController.java
@@ -1,21 +1,14 @@
-package dppd.source.backend.demo.demo.src.main.java.com.example.demo.controllers;
+package com.example.demo.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-
-//@RestController
-//public class HelloController {
-//    @GetMapping("/hello")
-//    public String sayHello() {
-//        return "Hello, World!";
-//    }
-//}
 
 @Controller
-public class FrontendController {
+public class HelloController {
+
     @GetMapping("/main")
     public String mainPage() {
+
         return "main";
     }
 }
