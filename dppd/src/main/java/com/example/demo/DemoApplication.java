c20d2784f0b9db1451897f6e70ff08e3e763a0f1
arian <arian.esmkhani@gmail.com>
2025-02-28 14:01:56 +0330

    dppd-ub1-8

--- dppd/src/main/java/com/example/demo/DemoApplication.java
+++ dppd/src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,10 @@ package com.example.demo
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +14,12 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+}
+
+@Configuration
+public class ServerConfig {
+	@Bean
+	public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> webServerFactoryCustomizer() {
+		return factory -> factory.setPort(Integer.parseInt(System.getenv().getOrDefault("PORT", "8080")));
+	}
 }
